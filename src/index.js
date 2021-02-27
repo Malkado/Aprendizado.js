24834694babf9b2d825eabd676534de0a30850a4
Luiz Henrique <luizhsc.araujo@outlook.com>
2021-02-27 11:58:12 -0300

    refactor: variable name on env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = express();
 const router = require('./routes/index');
 //definição da porta
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(router);
 //Informações sobre o servidor
