50ecffd74304c76146b34ddae8f237046d9ac898
Luiz Henrique <luizhsc.araujo@outlook.com>
2021-02-27 10:47:06 -0300

    chore: create routes module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-
-app.use(express.json());
+const router = require('./routes/index');
 //definição da porta
 const port = process.env.SERVER_PORT || 3000;
-
+app.use(express.json());
+app.use(router);
 //Informações sobre o servidor
 app.listen(port, () => {
     console.log(`Listening on the port: ${port}`);
